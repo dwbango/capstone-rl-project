39da15c512607ccd6dde6f22142c01f30b05040d
dbango <donaldbango11@aol.com>
2025-02-20 05:38:20 -0800

    fixing connection error

--- worker.py
+++ worker.py
@@ -1,13 +1,16 @@
 # worker.py
+
 import os
 import redis
-from rq import Worker, Queue, Connection
+from rq import Worker, Queue
 
 redis_url = os.getenv('REDISCLOUD_URL', 'redis://localhost:6379')
 conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
-    with Connection(conn):
-        qs = [Queue('default')]
-        w = Worker(qs)
-        w.work()
+    # Create a queue *with* the explicit connection
+    default_queue = Queue('default', connection=conn)
+
+    # Create a worker *with* the explicit connection
+    w = Worker([default_queue], connection=conn)
+    w.work()
